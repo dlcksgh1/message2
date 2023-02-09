37f2227036253d333308db9f3ddfaf59b9f57304
Lee  Chanho <sma05091@nate.com>
2023-02-09 21:53:49 +0900

    스프링 MessageSource 사용 , 테스트
    테스트 도중 기본, Locale.KOREA 일때가 안되고 Locale.ENGLISH 일때는 되는 현상이 발생. 인코딩 문제라고 생각하지 못했음...
    setting - fileEncodings UTF-8 로 변경

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
